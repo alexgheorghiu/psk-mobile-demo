413c2a52a9c21af1d574c1d77fd1563b1e56b1f4
Deny Pătrașcu <denypatrascu@gmail.com>
2020-09-22 13:42:13 +0300

    mobile banking demo

--- scripts/controllers/MainController.js
+++ scripts/controllers/MainController.js
@@ -2,15 +2,17 @@ import ContainerController from '../../cardinal/controllers/base-controllers/Con
 
 class MainController extends ContainerController {
     getModel() {
-        return {
+        const model = {
             footer: [
                 {
                     name: 'Balance',
-                    icon: 'balance-scale'
+                    icon: 'balance-scale',
+                    page: 'balance'
                 },
                 {
                     name: 'Payments',
-                    icon: 'money'
+                    icon: 'money',
+                    page: 'payments'
                 },
                 {
                     name: 'Services',
@@ -20,19 +22,77 @@ class MainController extends ContainerController {
                     name: 'Account',
                     icon: 'user'
                 }
-            ]
+            ],
+            fullscreen: {
+                event: 'toggle-fullscreen',
+                in: {
+                    icon: 'arrows-alt',
+                    text: 'Enter fullscreen'
+                },
+                out: {
+                    icon: 'times',
+                    text: 'Exit fullscreen'
+                }
+            },
+            options: {
+                in: {
+                    event: 'show-mobile-options'
+                },
+                out: {
+                    event: 'hide-mobile-options'
+                }
+            }
+
         }
+        model.fullscreen.option = model.fullscreen.in;
+        return model;
     }
 
     constructor(element) {
         super(element);
         this.model = this.setModel(this.getModel());
 
-        // this.on("needMenuItems", e => {
-        //     e.stopImmediatePropagation();
-        //     const callback = e.detail;
-        //     callback(null, this.model.sidebar);
-        // });
+        this.checkFullScreen();
+
+        this.on(this.model.fullscreen.event, async e => {
+            e.preventDefault();
+            e.stopImmediatePropagation();
+            await this.toggleFullScreen();
+        });
+    }
+
+
+
+    async toggleFullScreen() {
+        if (!document.fullscreenElement) {
+            try {
+                await document.documentElement.requestFullscreen();
+                this.model.fullscreen.option = this.model.fullscreen.out;
+                this.hideOptions();
+            } catch (err) {
+                console.error('Fullscreen error [in]:', err);
+            }
+        } else {
+            if (document.exitFullscreen) {
+                try {
+                    await document.exitFullscreen();
+                    this.model.fullscreen.option = this.model.fullscreen.in;
+                    this.hideOptions();
+                } catch (err) {
+                    console.error('Fullscreen error [out]:', err);
+                }
+            }
+        }
+    }
+
+    checkFullScreen() {
+        if (document.fullscreenEnabled) {
+            this.model.fullscreen.option = this.model.fullscreen.out;
+        }
+    }
+
+    hideOptions() {
+        document.dispatchEvent(new Event(this.model.options.out.event));
     }
 }
 
